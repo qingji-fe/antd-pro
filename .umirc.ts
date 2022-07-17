42a527a9a683b8fc7c0cf331fb16b701912d2e3f
qingji-xdy <1809879974@qq.com>
2022-07-17 22:22:34 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'antd-pro',
+  favicon:
+    'https://gw.alipayobjects.com/zos/antfincdn/upvrAjAPQX/Logo_Tech%252520UI.svg',
+  logo: 'https://gw.alipayobjects.com/zos/antfincdn/upvrAjAPQX/Logo_Tech%252520UI.svg',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  // more config: https://d.umijs.org/config
+});
---
+++ src/FilterTable/index.md
@@ -0,0 +1,12 @@
+---
+title: 筛选表格
+nav:
+  title: 组件
+  path: /components
+group:
+  title: 数据展示
+---
+
+# FilterTable
+
+## 基本使用
