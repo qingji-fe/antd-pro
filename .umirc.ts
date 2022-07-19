730a328b56af86d23bcbbaa422f15a1901fa68b5
qingji-xdy <1809879974@qq.com>
2022-07-19 09:34:28 +0800

    style: logo

--- .umirc.ts
+++ .umirc.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'antd-pro',
   favicon:
-    'https://gw.alipayobjects.com/zos/antfincdn/upvrAjAPQX/Logo_Tech%252520UI.svg',
-  logo: 'https://gw.alipayobjects.com/zos/antfincdn/upvrAjAPQX/Logo_Tech%252520UI.svg',
+    'https://avatars.githubusercontent.com/u/108705856?s=200&v=4',
+  logo: 'https://avatars.githubusercontent.com/u/108705856?s=200&v=4',
   outputPath: 'docs-dist',
   mode: 'site',
   // more config: https://d.umijs.org/config
